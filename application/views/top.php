73a84735375de5fe24a7f50e3e08034cae707c19
miumiu0917 <miumiu917@gmail.com>
2016-02-06 10:41:24 +0900

    logファイル設定

--- application/views/top.php
+++ application/views/top.php
@@ -2,40 +2,41 @@
 $base_url = $this->config->site_url();
  ?>
 
+ <!doctype html>
+ <html lang="en-US">
 <html>
-       <head>
-           <meta http-equiv="content-type" content="text/html; charset=utf-8">
-           <title>ログイン</title>
-       </head>
-       <body>
-           <h1>ログイン</h1>
-           <hr>
-           <div align="center">
-              <table border="0">
-                  <form action="<?php echo $base_url."/login"; ?>" method="POST">
-                                         <tr>
-                                                                    <th>
-                              ユーザID
-                          </th>
-                          <td>
-                              <input type="text" name="user_id" value="" size="24">
-                          </td>
-                                             </tr>
-                      <tr>
-                          <th>
-                              パスワード
-                          </th>
-                          <td>
-                              <input type="password" name="password" value="" size="24">
-                          </td>
-                                             </tr>
-                      <tr>
-                          <td colspan="2">
-                              <input type="submit" value="ログイン">
-                          </td>
-                      </tr>
-                  </form>
-              </table>
-          </div>
-      </body>
-  </html>
+<head>
+   <meta http-equiv="content-type" content="text/html; charset=utf-8">
+   <title>films</title>
+</head>
+<body>
+   <h1>サインイン</h1>
+   <div align="center">
+      <table border="0">
+          <form action="<?php echo $base_url."/login"; ?>" method="POST">
+              <tr>
+                  <th>
+                      ユーザID
+                  </th>
+                  <td>
+                      <input type="text" name="user_id" value="" size="24">
+                  </td>
+              </tr>
+              <tr>
+                  <th>
+                      パスワード
+                  </th>
+                  <td>
+                      <input type="password" name="password" value="" size="24">
+                  </td>
+              </tr>
+              <tr>
+                  <td colspan="2">
+                      <input type="submit" value="サインイン">
+                  </td>
+              </tr>
+          </form>
+      </table>
+  </div>
+</body>
+</html>
