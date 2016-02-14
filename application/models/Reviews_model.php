c27d4eff8dbc315386d635c8225c7f95bf14f205
Shogo Sekine <gan.gun.kyosho@gmail.com>
2016-02-14 11:03:21 +0900

    Reviewsの再現

---
+++ application/models/Reviews_model.php
@@ -0,0 +1,25 @@
+<?php
+class Review_model extends CI_Model {
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    function main($movie_id)
+    {
+      $sql = "SELECT
+       user_id,
+       date,
+       contents
+       FROM reviews
+       WHERE movie_id = ?
+       ORDER BY date DESC
+      ";
+
+      $result = $this->db->query($sql,$movie_id)->result_array();
+      return $result;
+
+    }
+}
