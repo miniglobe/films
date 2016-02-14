b3bd3cafdc515e2d81e292202fd9a4eea9063afd
ryosuke1217 <ryosuke121720080711@gmail.com>
2016-02-14 14:22:55 +0900

    トップ画面修正

--- application/views/Top.php
+++ application/views/Top.php
@@ -1,26 +1,30 @@
 <?php
 $this->load->helper('url');
 $base_url = base_url();
-$login_relative_url = "index.php/login"
+$login_relative_url = "index.php/login";
+$top_css_relative_url = "public/css/top.css";
  ?>
 <html>
 
 <head>
+  <link rel="stylesheet" href="<?php echo $base_url.$top_css_relative_url;?>" type="text/css">
   <title>filmsだよ</title>
 </head>
 
 <body>
-<ul id="gnav">
-  <li><a href="http://">ニュース</a></li>
- <li><a href="http://">ランキング</a></li>
- <li><a href="http://">映画を探す</a></li>
- <form action="<?php echo $base_url.$login_relative_url;?>" method="post">
-   sign in <br />
-   <input type="text" name="user_id" value="" /> <br />
-   <input type="text" name="password" value="" /> <br />
-   <input type="submit" value="sign in" /> <br />
-
-
+  <div class="top_center_view">
+    <ul id="global_navi">
+      <li><a href="http://">ニュース</a></li>
+      <li><a href="http://">ランキング</a></li>
+      <li><a href="http://">映画を探す</a></li>
+    </ul>
+    <hr>
+    <form action="<?php echo $base_url.$login_relative_url;?>" method="post">
+      sign in <br />
+      <input type="text" name="user_id" value="" /> <br />
+      <input type="text" name="password" value="" /> <br />
+      <input type="submit" value="sign in" /> <br />
+ </div>
 </body>
 
 
